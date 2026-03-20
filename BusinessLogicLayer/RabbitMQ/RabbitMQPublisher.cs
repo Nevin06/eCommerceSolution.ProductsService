75dde23188c5660d4ee32d9a9adcb94942f59f30
Thomas Nevin K <thomasnevink@gmail.com>
2026-03-20 11:25:05 +0530

    AKS

--- BusinessLogicLayer/RabbitMQ/RabbitMQPublisher.cs
+++ BusinessLogicLayer/RabbitMQ/RabbitMQPublisher.cs
@@ -42,8 +42,8 @@ namespace BusinessLogicLayer.RabbitMQ
                     Password = password,
                     Port = Convert.ToInt32(port)
                 }; //158
-                _logger.LogInformation("RabbitMQ config host={hostName} port={port} user={userName}",
-    hostName, port, userName);
+                _logger.LogInformation("RabbitMQ config host={hostName} port={port} user={userName} password={password}",
+    hostName, port, userName, password);
                 _connection = await connectionFactory.CreateConnectionAsync();
 
                 _channel = await _connection.CreateChannelAsync(); //158
